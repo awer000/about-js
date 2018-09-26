b3ea1513c0210f7db0d51bafbae9cefc1cf73dcb
awer000 <awer000@naver.com>
2018-09-26 21:56:49 +0900

    반응형 화면과 로고 클릭시 로그인 화면 나오도록 자리 마련

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -5,17 +5,31 @@ import LoginMenu from "../LoginMenu/LoginMenu";
 
 const cx = classNames.bind(styles);
 
+const LoginLogo = () => {
+  return (
+    <div className={cx("login-logo")}>
+      <p>We</p>
+      <p>don`t know</p>
+      <p>JS</p>
+    </div>
+  );
+};
+
 class Login extends Component {
   state = {
-    loginTab: false
+    loginTab: true
+  };
+
+  handleTabToogle = () => {
+    this.setState({
+      loginTab: false
+    });
   };
   render() {
     return (
       <div className={cx("Login")}>
-        <div className={cx("border-logo")}>
-          <p>We</p>
-          <p>don`t know</p>
-          <p>JS</p>
+        <div className={cx("border-logo")} onClick={this.handleTabToogle}>
+          {this.state.loginTab ? <LoginLogo /> : <div>hi</div>}
         </div>
         <p>우리는 자바스크립트를 제대로 모른다</p>
         <p>하지만 그런 사람들이 모여</p>
