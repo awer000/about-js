bd6f98dea63c09a5320f0c116a407001be1a73c5
awer000 <awer000@naver.com>
2018-09-26 20:52:15 +0900

    정리중

--- src/index.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import registerServiceWorker from "./registerServiceWorker";
-import Root from "./Root";
-import "styles/base.scss";
-
-ReactDOM.render(<Root />, document.getElementById("root"));
-registerServiceWorker();
--- src/pages/index.js
+++
@@ -1,5 +0,0 @@
-export { default as EditorPage } from "./EditorPage";
-export { default as LoginPage } from "./LoginPage";
-export { default as MainPage } from "./MainPage";
-export { default as PostPage } from "./PostPage";
-export { default as NotFoundPage } from "./NotFoundPage";
